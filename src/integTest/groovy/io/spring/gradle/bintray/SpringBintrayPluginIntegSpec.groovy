6ccfea238826537e1e270e1021f323cd58f75db4
Jon Schneider <jschneider@pivotal.io>
2018-05-03 13:59:18 -0500

    Polish

--- src/integTest/groovy/io/spring/gradle/bintray/SpringBintrayPluginIntegSpec.groovy
+++ src/integTest/groovy/io/spring/gradle/bintray/SpringBintrayPluginIntegSpec.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2018 Pivotal Software, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.spring.gradle.bintray
 
 import nebula.test.IntegrationSpec
@@ -5,8 +20,11 @@ import nebula.test.IntegrationSpec
 class SpringBintrayPluginIntegSpec extends IntegrationSpec {
     def 'publish a multi-module project to JCenter'() {
         when:
+        def propsInput = getClass().getResourceAsStream('/bintray.properties')
+
         def props = new Properties()
-        props.load(getClass().getResourceAsStream('/bintray.properties'))
+        if(propsInput)
+            props.load(propsInput)
 
         buildFile << """\
             allprojects {
@@ -65,8 +83,13 @@ class SpringBintrayPluginIntegSpec extends IntegrationSpec {
         '''.stripMargin()
 
         then:
-        def result = runTasksSuccessfully('bintrayPublish')
-        println(result.standardOutput)
-        println(result.standardError)
+
+        if(propsInput) {
+            def result = runTasksSuccessfully('bintrayPublish')
+            println(result.standardOutput)
+        }
+        else {
+            println("Skipping integration test. Provide a bintray.properties in integTest/resources that includes bintrayUser, bintrayKey, and gpgPassphrase to run.")
+        }
     }
 }
